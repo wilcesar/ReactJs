1ede42bc2782d2a2d6aa966ffb1572a0356a98d2
Wilian Cesar <wilcesar7@gmail.com>
2020-03-01 21:32:00 -0300

    Trabalhando com states
    Aula 33

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      nome: 'Mateus',
+      contador:0
+    };
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+  }
+  aumentar(){
+    let state = this.state;
+    state.contador += 1;
+    state.nome = "Jose"
+
+    this.setState(state)
+  }
+  diminuir(){
+    let state = this.state;
+    if(state.contador === 0){
+      alert('Opa chegou a zero!');
+      return;
+    }
+    state.contador -= 1;
+
+    this.setState(state)
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Contador</h1>
+        {this.state.nome}
+        <h3>
+          <button onClick={this.diminuir}>-</button>
+          {this.state.contador}
+          <button onClick={this.aumentar}>+</button>
+        </h3>
+      </div>
+    )
+  }
 }
 
 export default App;
