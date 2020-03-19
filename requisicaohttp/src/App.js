0086a2bba803b580ec8a0188f1dbfb1bff8f3026
Wilian Cesar <wilcesar@live.com>
2020-03-18 21:38:07 -0300

    Aula 45 - Requisições HTTP

---
+++ requisicaohttp/src/App.js
@@ -0,0 +1,45 @@
+import React,{ Component} from 'react';
+import './style.css'
+class App extends Component {
+
+  constructor (props){
+    super(props);
+    this.state = {};
+    this.state = {
+      nutri:[]
+    }
+  }
+  componentDidMount(){
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts'
+    fetch(url)
+    .then((r)=>r.json())
+    .then((json)=>{
+      let state = this.state;
+      state.nutri = json;
+      this.setState(state);
+      //console.log(json);
+    })
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <header>
+          <strong>React Nutri</strong>
+        </header>
+        {this.state.nutri.map((item)=>{
+          return(
+            <article key={item.id} className="post">
+              <strong className="titulo"> {item.titulo}  </strong>
+              <img src={item.capa} className="capa" />
+              <p className="subtitulo"> {item.subtitulo} </p>
+              <a className='botao' href="#">Acessar</a>
+            </article>
+          )
+        })}
+      </div>
+    )
+  }
+};
+
+export default App;
